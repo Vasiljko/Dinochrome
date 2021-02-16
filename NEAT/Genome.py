8bfbe9c95e5fb72debd4ee959e61e8f6f5cbac46
Petar Vasiljevic <petarvasiljevic2001@gmail.com>
2021-02-16 12:49:33 +0100

    Update Genome.py

--- NEAT/Genome.py
+++ NEAT/Genome.py
@@ -5,8 +5,8 @@ import numpy as np
 import random
 
 
-# TODO: Crossover and compatibility distance should be changed (current complexity is O(NlogN) where N is number of ConnectionGenes
-# TODO: In order to decrease it to linear complexity, the way of storing ConenctionGenes should be changed, i.e. we should use dictionaries
+# FIXED: Crossover and compatibility distance should be changed (current complexity is O(NlogN) where N is number of ConnectionGenes
+# FIXED: In order to decrease it to linear complexity, the way of storing ConenctionGenes should be changed, i.e. we should use dictionaries
 
 # Changed getConnections() and getNodes(). They are not using deepcopy anymore
 
@@ -14,7 +14,8 @@ import random
 class Genome:
 
     def __init__(self):
-        self.connections = []   # List of connections -> using ConnectionGene class
+        self.connections = {}   # Dictionary of connections -> using ConnectionGene class
+                                # { Innovation number: ConnectionGene }
 
         self.nodes = []         # List of nodes -> using NodeGene class
 
@@ -25,13 +26,13 @@ class Genome:
         return new_nodes
 
     def getConnections(self):
-        new_connections = []
-        for connection in self.connections:
-            new_connections.append(ConnectionGene(connection.in_node, connection.out_node, connection.weight, connection.expressed, connection.innovation_number))
+        new_connections = {}
+        for connection in self.connections.values():
+            new_connections[connection.innovation_number] = ConnectionGene(connection.in_node, connection.out_node, connection.weight, connection.expressed, connection.innovation_number)
         return new_connections
 
     def addNode(self, node):
-        self.nodes.append(node)
+        self.nodes.append(NodeGene(node.type, node.id))
 
     def addConnection(self, connection):
         self.connections[connection.innovation_number] = ConnectionGene(connection.in_node, connection.out_node, connection.weight, connection.expressed, connection.innovation_number)
@@ -43,13 +44,13 @@ class Genome:
 
         # 90% chance for weight to be uniformly perturbed
         # 10% chance of being assigned a new value
-        for connection in self.connections:
+        for innovation in self.connections.keys():
             if random.random()<0.9:
-                connection.weight = connection.weight*(4*random.random() - 2)
+                self.connections[innovation].weight = self.connections[innovation].weight*(4*random.random() - 2)
             else:
-                connection.weight = 4*random.random() - 2
+                self.connections[innovation].weight = 4*random.random() - 2
 
-    def add_connection(self,max_iterations):
+    def add_connection(self,max_iterations = 100):
         iteration = 0
 
         # It also breaks when connection is found
@@ -82,10 +83,10 @@ class Genome:
 
             connection_exists = False
 
-            for connection in self.connections:
-                if connection.getInNode() == node1.getId() and connection.getOutNode() == node2.getId():
+            for connection in self.connections.values():
+                if connection.in_node.id == node1.getId() and connection.out_node.id == node2.getId():
                     connection_exists = True
-                elif connection.getInNode() == node2.getId() and connection.getOutNode() == node1.getId():
+                elif connection.in_node.id == node2.getId() and connection.out_node.id == node1.getId():
                     connection_exists = True
 
 
@@ -103,8 +104,8 @@ class Genome:
             break
 
     def add_node(self):
-        random_connection_index = random.randint(0,len(self.connections)-1)
-        connection = self.connections[random_connection_index]
+        innovation = random.choice(list(self.connections.keys()))
+        connection = self.connections[innovation]
 
         inNode = connection.getInNode()
         outNode = connection.getOutNode()
@@ -113,17 +114,18 @@ class Genome:
         # Disable current conenction
         connection.expressed = False
 
-
         # Add node in hidden layer -> type = 1
         id = len(self.nodes)+1
         self.nodes.append(NodeGene(1, id))
 
 
         # Add connection start_node -> new_one
-        self.connections.append(ConnectionGene(inNode, id, 1.0, True, Innovation.getNewInnovation(inNode, id)))
+        new_innovation = Innovation.getNewInnovation(inNode.id, id)
+        self.connections[new_innovation] = ConnectionGene(inNode.id, id, 1.0, True, new_innovation)
 
         # Add connection new_one -> end_node
-        self.connections.append(ConnectionGene(id, outNode, current_weight, True, Innovation.getNewInnovation(id, outNode)))
+        new_innovation = Innovation.getNewInnovation(inNode.id, id)
+        self.connections[new_innovation] = ConnectionGene(id, outNode.id, current_weight, True, new_innovation)
 
 
     # Assuming that Parent 1 is fitter than Parent 2
@@ -134,44 +136,24 @@ class Genome:
         for node in nodes1:
             child.addNode(node)
 
-        connections1 = sorted(parent1.getConnections())
-        connections2 = sorted(parent2.getConnections())
+        for innovation in parent1.connections.keys():
+            if innovation in parent2.connections:    # Matching genes
+                connection1 = parent1.connections[innovation]
+                connection2 = parent2.connections[innovation]
 
-        # Do the Crossover
-        i,j = 0,0
-
-        while i<len(connections1) or j<len(connections2):
-            if i == len(connections1):
-                # Do nothing, because Parent 1 is fitter than parent 2
-                j+=1
-            elif j == len(connections2):
-                # Excess Gene of Parent 1
-                child.addConnection(connections1[i])
-                i+=1
-            else:
-                if connections1[i].getInnovation() == connections2[j].getInnovation():
-                    if (connections1[i].isExpressed() and connections2[j].isExpressed()) or (not connections1[i].isExpressed() and not connections2[j].isExpressed()):
-                        r = round(random.random())
-                        child.addConnection(connections1[i] if r==0 else connections2[j])
-                    else:
-                        r = random.random()
-
-                        # 75% change of inheriting disabled connection gene
-                        if r<0.75:
-                            child.addConnection(connections1[i] if not connections1[i].isExpressed() else connections2[j])
-                        else:
-                            child.addConnection(connections1[i] if connections1[i].isExpressed() else connections2[j])
-
-                    i+=1
-                    j+=1
-                elif connections1[i].getInnovation() < connections2[j].getInnovation():
-                    # Disjoint Gene of Parent 1
-                    child.addConnection(connections1[i])
-
-                    i+=1
+                if (connection1.isExpressed() and connection2.isExpressed()) or (not connection1.isExpressed() and not connection2.isExpressed()):
+                    r = round(random.random())
+                    child.addConnection(connection1 if r == 0 else connection2)
                 else:
-                    # Do nothing, because Parent 1 is fitter than Parent 2
-                    j+=1
+                    r = random.random()
+
+                    # 75% change of inheriting disabled connection gene
+                    if r < 0.75:
+                        child.addConnection(connection1 if not connection1.isExpressed() else connection2)
+                    else:
+                        child.addConnection(connection1 if connection1.isExpressed() else connection2)
+            else:
+                child.addConnection(parent1.connections[innovation])
 
         return child
 
@@ -179,40 +161,30 @@ class Genome:
     def compatibility_distance(self, g1, c1, c2, c3):
         matching, disjoint, excess, weight = 0, 0, 0, 0
 
-        i, j = 0, 0
-        connections1 = sorted(self.getConnections())
-        connections2 = sorted(g1.getConnections())
+        minimum_innovation1 = max(list(self.connections.keys()))
+        minimum_innovation2 = max(list(g1.connections.keys()))
+        minimum_innovation = min(minimum_innovation1, minimum_innovation2)
 
-        while i < len(connections1) or j < len(connections2):
-            if i == len(connections1):  # No gene from g2 can be matched
-                excess += 1
-                j += 1
-            elif j == len(connections2):  # No gene from g1 can be matched
-                excess += 1
-                i += 1
+        for innovation in self.connections.keys():
+            if innovation in g1.connections:
+                matching += 1
+                weight += abs(self.connections[innovation].weight - g1.connections[innovation].weight)
+            elif innovation <= minimum_innovation:
+                disjoint += 1
             else:
-                if connections1[i].getInnovation() == connections2[j].getInnovation():
-                    matching += 1
-                    weight += abs(connections1[i].weight - connections2[j].weight)
+                excess += 1
 
-                    i += 1
-                    j += 1
-                elif connections1[i].getInnovation() < connections2[j].getInnovation():
-                    disjoint += 1
-                    i += 1
-                else:
-                    disjoint += 1
-                    j += 1
+        for innovation in g1.connections.keys():
+            if innovation in self.connections:
+                pass
+            elif innovation <= minimum_innovation:
+                disjoint += 1
+            else:
+                excess += 1
 
         N = max(len(self.connections), len(g1.connections))
         N = 1 if N < 20 else N
 
         W = weight / matching if matching != 0 else 0
 
-        print(matching, disjoint, excess)
         return c1 * excess / N + c2 * disjoint / N + c3 * W
-
-
-
-
-
