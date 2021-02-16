2782df5d6e98970ceb361406cc7159764b1104d3
Petar Vasiljevic <petarvasiljevic2001@gmail.com>
2021-02-16 12:50:05 +0100

    Update NodeGene.py

--- NEAT/NodeGene.py
+++ NEAT/NodeGene.py
@@ -1,3 +1,16 @@
+from ConnectionGene import ConnectionGene
+import numpy as np
+
+
+#TODO: connections_in should be filled in Genome class
+#TODO: Maybe some more calculate functions should be implemented
+
+#TODO: Should find a way to calculate all the outputs for each NodeGene and then calculate final outputs
+# But, what about reccurent connections??
+# Should implement distance from input nodes, and then distance is equal to maximum value of
+# previous node's distance + 1
+
+
 class NodeGene:
     def __init__(self, type, id):
         #input node  -> 0
@@ -6,14 +19,33 @@ class NodeGene:
         self.type = type
         self.id = id
 
+        self.connections_in = []    # List of ConnectionGenes
+        self.output = 0
+
+
     def __eq__(self, other):
         return self.type == other.type and self.id == other.id
+
+    def calculate(self):
+        s = 0
+        for connection in self.connections_in:
+            if connection.expressed == True:
+                s += connection.in_node.output * connection.weight
+
+        self.output = self.activation_function(s)
+
+
+    def activation_function(self, x):
+        return 1/(1+np.exp(-4.9*x))
 
     def getType(self):
         return self.type
 
     def getId(self):
         return self.id
+
+    def copyNode(self):
+        return NodeGene(self.type, self.id)
 
     def Print(self):
         print(str(self.type)+" "+str(self.id))
